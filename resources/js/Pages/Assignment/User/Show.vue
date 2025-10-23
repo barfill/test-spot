a9aec8b8fdd56c7396856cf84f41d65e8b42a6d2
BartekF <bartosz_filipczak@op.pl>
2025-10-23 16:21:08 +0200

    Minor fix

--- resources/js/Pages/Assignment/User/Show.vue
+++ resources/js/Pages/Assignment/User/Show.vue
@@ -88,7 +88,9 @@
             </div>
         </Card>
 
-
+        <Card class="bg-white dark:bg-zinc-700 rounded-lg shadow p-6 mb-6">
+            <h3 class="text-lg font-semibold mb-4">Errors</h3>
+        </Card>
 
 
         <Card class="bg-white dark:bg-zinc-700 rounded-lg shadow p-6 mb-6">
